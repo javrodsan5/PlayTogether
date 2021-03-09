5ef8e0fd23b8ef20d678810e33011808b144830e
carsantir99 <carsantir@gmail.com>
2021-03-09 21:56:21 +0100

    Creado proyecto inicial en el repositorio

---
+++ src/test/java/com/playtogether/playtogether/PlaytogetherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.playtogether.playtogether;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaytogetherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
