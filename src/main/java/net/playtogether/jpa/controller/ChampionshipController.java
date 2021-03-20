c33eff62c8a3b7872b77f86c1b73f020de5bd44e
Andrés Fernández Rodríguez <andferrod3@alum.us.es>
2021-03-20 11:18:55 +0100

    Eliminación de importaciones innecesarias

--- src/main/java/net/playtogether/jpa/controller/ChampionshipController.java
+++ src/main/java/net/playtogether/jpa/controller/ChampionshipController.java
@@ -1,31 +1,22 @@
 package net.playtogether.jpa.controller;
 
 import java.util.Collection;
-import java.util.List;
-import java.util.Map;
 
-import javax.activity.InvalidActivityException;
-import javax.validation.Valid;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataAccessException;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+
 
 import net.playtogether.jpa.entity.Championship;
 import net.playtogether.jpa.entity.Sport;
-import net.playtogether.jpa.entity.User;
-import net.playtogether.jpa.repository.UserRepository;
 import net.playtogether.jpa.service.ChampionshipService;
 
 @Controller
