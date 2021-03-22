fbbea1cb860d3529c3327b6ffc82f714288309e0
Manuel Bueno <manbuegom@alum.us.es>
2021-03-22 14:17:38 +0100

    Task 005 - Manbuegom/Fercadu: Participar en quedada & tests - FINISHED

--- src/test/java/net/playtogether/jpa/web/MeetingTests.java
+++ src/test/java/net/playtogether/jpa/web/MeetingTests.java
@@ -1,4 +1,4 @@
-package net.playtogether.jpa.tests;
+package net.playtogether.jpa.web;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -19,6 +19,7 @@ import org.springframework.security.web.servlet.support.csrf.CsrfRequestDataValu
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
@@ -40,15 +41,15 @@ public class MeetingTests {
 	  private MeetingRepository meetingRepository;
 	  
 	//Test de consultar quedadas
-//	@Test
-//	void listMeetings() throws Exception{
-//		this.mockMvc.perform(get("/meetings"))
-//		.andExpect(status().is2xxSuccessful());
-//		
-//		Collection<Meeting> meetingEntities = meetingRepository.findAll();
-//		assertThat(meetingEntities.size()).isEqualTo(3);
-//	
-//		}
+	@Test
+	void listMeetings() throws Exception{
+		this.mockMvc.perform(get("/meetings"))
+		.andExpect(status().is2xxSuccessful());
+		
+		Collection<Meeting> meetingEntities = meetingRepository.findAll();
+		assertThat(meetingEntities.size()).isEqualTo(2);
+	
+		}
 //	  
 //	 //Test de crear quedada
 //	@Test
@@ -78,5 +79,16 @@ public class MeetingTests {
 //		
 //		
 //	}
-
+	  
+//	//Test join meeting controller
+//		@Test
+//		@Transactional
+//		void joinMeeting() throws Exception{
+//			this.mockMvc.perform(get("/meetings/2/join")).andExpect(status().is2xxSuccessful());
+//			
+//			
+//			Meeting meetingEntity = meetingRepository.findById(2).orElse(null);
+//			assertThat(meetingEntity.getParticipants().size()).isEqualTo(1);
+//			
+//		}
 }
