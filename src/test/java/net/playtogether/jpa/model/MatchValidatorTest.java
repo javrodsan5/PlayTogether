5f373cae521cbde02a591ec9b7c8e7293225f907
Gonzalo <gonguelun@alum.us.es>
2021-04-01 14:18:54 +0200

    Task 012: Tests de indicar resultado de partido

---
+++ src/test/java/net/playtogether/jpa/model/MatchValidatorTest.java
@@ -0,0 +1,94 @@
+package net.playtogether.jpa.model;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Locale;
+import java.util.Set;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
+
+import net.playtogether.jpa.entity.Championship;
+import net.playtogether.jpa.entity.Match;
+import net.playtogether.jpa.entity.Meeting;
+import net.playtogether.jpa.entity.Sport;
+import net.playtogether.jpa.entity.SportType;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MatchValidatorTest {
+
+	private Validator createValidator() {
+		LocalValidatorFactoryBean localValidatorFactoryBean = new LocalValidatorFactoryBean();
+		localValidatorFactoryBean.afterPropertiesSet();
+		return localValidatorFactoryBean;
+	}
+
+	@Test
+	void shouldNotValidateWhenDateNull() {
+		LocaleContextHolder.setLocale(Locale.ENGLISH);
+		Championship championship = new  Championship();
+		
+		Sport sport = new Sport();
+
+		sport.setName("Surf");
+		sport.setSportType(new SportType());
+		sport.setMeetings(new ArrayList<Meeting>());
+		sport.setChampionships(new ArrayList<Championship>());
+
+		championship.setCity("Sevilla");
+		championship.setFinishDate(LocalDate.of(2022, 3, 4));
+		championship.setStartDate(LocalDate.of(2022, 2, 4));
+		championship.setSport(sport);
+		championship.setMaxTeams(8);
+		championship.setDescription(null);
+
+		Match match = new Match();
+		
+		match.setChampionship(championship);
+	
+		Validator validator = createValidator();
+		Set<ConstraintViolation<Match>> constraintViolations = validator.validate(match);
+		assertThat(constraintViolations.size()).isEqualTo(1);
+		ConstraintViolation<Match> violation = constraintViolations.iterator().next();
+		assertThat(violation.getPropertyPath().toString()).isEqualTo("dateTime");
+		assertThat(violation.getMessage()).isEqualTo("must not be null");
+		
+	}
+	
+
+	void shouldNotValidateWhenFinishDateNull() {
+		LocaleContextHolder.setLocale(Locale.ENGLISH);
+		Championship championship = new  Championship();
+		
+		Sport sport = new Sport();
+
+		sport.setName("Surf");
+		sport.setSportType(new SportType());
+		sport.setMeetings(new ArrayList<Meeting>());
+		sport.setChampionships(new ArrayList<Championship>());
+
+		championship.setCity("Sevilla");
+		championship.setFinishDate(null);
+		championship.setStartDate(LocalDate.of(2022, 2, 4));
+		championship.setSport(sport);
+		championship.setMaxTeams(8);
+		championship.setDescription("hola es una prueba");
+
+	
+		Validator validator = createValidator();
+		Set<ConstraintViolation<Championship>> constraintViolations = validator.validate(championship);
+		assertThat(constraintViolations.size()).isEqualTo(1);
+		ConstraintViolation<Championship> violation = constraintViolations.iterator().next();
+		assertThat(violation.getPropertyPath().toString()).isEqualTo("finishDate");
+		assertThat(violation.getMessage()).isEqualTo("must not be null");
+		
+	}
+	
+
+}
---
+++ src/test/java/net/playtogether/jpa/service/MatchServiceTests.java
@@ -0,0 +1,70 @@
+package net.playtogether.jpa.service;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import net.playtogether.jpa.entity.Championship;
+import net.playtogether.jpa.entity.Match;
+import net.playtogether.jpa.entity.Team;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class MatchServiceTests {
+
+	@Autowired
+	private MatchService matchService;
+	
+	@Autowired
+	private ChampionshipService championshipService;
+	
+	@Autowired
+	private SportService sportService;
+
+	// FIND MATCH BY ID
+	@Test
+	void shouldFindMatchWithCorrectId() throws Exception {
+		Match match = this.matchService.findMatchById(1);
+		assertThat(match.getChampionship().getId()).isEqualTo(1);
+	}
+	
+	@Test
+	void shouldSaveMatch() throws Exception {
+		Championship championship = new  Championship();
+	
+		championship.setCity("Sevilla");
+		championship.setFinishDate(LocalDate.of(2022, 3, 4));
+		championship.setStartDate(LocalDate.of(2022, 2, 4));
+		championship.setSport(this.sportService.findSportById(1));
+		championship.setMaxTeams(8);
+		championship.setDescription("descripción");
+		
+		this.championshipService.save(championship);
+		
+		Team team1 = new Team();
+		team1.setChampionship(championship);
+		Team team2 = new Team();
+		team2.setChampionship(championship);
+		
+		this.championshipService.save(team1);
+		this.championshipService.save(team2);
+		
+		Match match = new Match();
+
+		match.setChampionship(championship);
+		match.setDateTime(LocalDateTime.now());
+		match.setTeam1(team1);
+		match.setTeam2(team2);
+		this.matchService.save(match);
+		Match match2 = this.matchService.findMatchById(match.getId());
+		assertThat(match.getId()).isEqualTo(match2.getId());
+	}
+	
+}
