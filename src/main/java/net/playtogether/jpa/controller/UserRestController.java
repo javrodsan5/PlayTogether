c1d337ae56047694d4dc7fa886432399dd3bb61f
Andrés Fernández Rodríguez <andferrod3@alum.us.es>
2021-04-07 16:47:08 +0200

    Caso de Uso 6 finalizado, funcionalidad implementada y tests realizados

--- src/main/java/net/playtogether/jpa/controller/UserRestController.java
+++ src/main/java/net/playtogether/jpa/controller/UserRestController.java
@@ -7,12 +7,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import net.playtogether.jpa.entity.Championship;
 import net.playtogether.jpa.entity.User;
 import net.playtogether.jpa.repository.UserRepository;
+import net.playtogether.jpa.service.UserService;
  
  
 @Controller
@@ -21,6 +25,10 @@ public class UserRestController {
  
     @Autowired
     UserRepository userRepository;
+    
+	@Autowired
+	UserService userService;
+
     
     @RequestMapping("/")
     public String home(ModelMap model) {
@@ -40,5 +48,14 @@ public class UserRestController {
     	model.addAttribute("usr",usr);
         return "welcome";
     }
+    
+    @GetMapping("/users/{userId}")
+	public String userDetails(final ModelMap model, @PathVariable("userId") final Integer userId) {
+		User user = this.userService.findUserById(userId);
+		model.addAttribute("user", user);
+
+
+		return "users/userDetails";
+	}
  
 }
