e84f20f7c22e8f761cf6ae96837d9dba9ba6bf03
carsantir99 <carsantir@gmail.com>
2021-04-07 23:25:10 +0200

    Registro finalizado

--- src/main/java/net/playtogether/jpa/controller/UserRestController.java
+++ src/main/java/net/playtogether/jpa/controller/UserRestController.java
@@ -1,17 +1,12 @@
 package net.playtogether.jpa.controller;
 
-import java.util.List;
  
  
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
-import net.playtogether.jpa.entity.Usuario;
 import net.playtogether.jpa.repository.UsuarioRepository;
  
  
@@ -25,20 +20,8 @@ public class UserRestController {
     @RequestMapping("/")
     public String home(ModelMap model) {
     	
-    	List<Usuario> usr=userRepository.findAll();
-    	model.addAttribute("usr",usr);
         return "welcome";
     }
     
-    @RequestMapping(value="/addUser", method = RequestMethod.POST)
-    public String addUser(@ModelAttribute("user") Usuario user, BindingResult result, ModelMap model) {
-        userRepository.save(user);
-        
-        String exit="Su correo ha sido añadido correctamente";
-        model.addAttribute("exit",exit);
-    	List<Usuario> usr=userRepository.findAll();
-    	model.addAttribute("usr",usr);
-        return "welcome";
-    }
  
 }
